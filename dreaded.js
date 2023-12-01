bbc954bd6cef08ac831d1fed946b2a5147ff669f
kimsirm <141669743+kimsirm@users.noreply.github.com>
2023-12-01 17:40:20 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -1322,7 +1322,7 @@ break;
 
 case "system": 
   
-              client.sendMessage(m.chat, { image: { url: 'https://i.imgur.com/eImS3eF.jpeg' }, caption:`𝐁𝐎𝐓 𝐍𝐀𝐌𝐄: 𝐂𝐑𝐎𝐖𝐍-𝐀𝐈\n\n𝐒𝐏𝐄𝐄𝐃: ${dreadedspeed.toFixed(4)} 𝐦𝐬\n\nRUNTIME: ${runtime(process.uptime())}`}); 
+              client.sendMessage(m.chat, { image: { url: 'https://i.imgur.com/eImS3eF.jpeg' }, caption:`𝐁𝐎𝐓 𝐍𝐀𝐌𝐄: 𝐂𝐑𝐎𝐖𝐍-𝐀𝐈\n\n𝐒𝐏𝐄𝐄𝐃: ${dreadedspeed.toFixed(4)} 𝐦𝐬\n\nRUNTIME: ${runtime(process.uptime())}\n\n𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌: Linux\n\n𝐇𝐎𝐒𝐓𝐍𝐀𝐌𝐄: Sirm\n\n𝐋𝐈𝐁𝐑𝐀𝐑𝐘: Baileys\n\n𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄: MongoDb\n\n𝐃𝐀𝐓𝐄: Undefined`}); 
  break; 
 
 case "take": {
