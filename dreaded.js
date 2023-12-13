2abc6e42b2be4d9dbe0b6d3f20e3e8bda186002c
kimsirm <141669743+kimsirm@users.noreply.github.com>
2023-12-13 18:02:55 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -102,7 +102,7 @@ const admin = process.env.ADMIN_MSG || '𝐅𝐚𝐢𝐥𝐮𝐫𝐞!, 𝐘𝐨�
     const group = process.env.GROUP_ONLY_MSG || 'Is this a group chat?';
     const botAdmin = process.env.BOT_ADMIN_MSG || '𝐅**𝐤 𝐲𝐨𝐮 🦄𝐚𝐦 𝐚𝐧 𝐧𝐨𝐭 𝐚𝐧 𝐚𝐝𝐦𝐢𝐧'
     const NotOwner = process.env.NOT_OWNER_MSG || 'Lol you are not 𝐈𝐭𝐬_𝐒𝐢𝐫𝐦™𝕏';
-const wapresence = process.env.WA_PRESENCE || 'typing';
+const wapresence = process.env.WA_PRESENCE || 'recording';
 const antilink = process.env.ANTILINK || 'TRUE';
 const antilinkall = process.env.ANTILINK_ALL || 'FALSE';
 
@@ -130,7 +130,7 @@ if (wapresence === 'recording' && !m.isGroup) {
   client.sendPresenceUpdate('recording', m.chat);
 } else if (wapresence === 'typing' && !m.isGroup) { 
             
-  client.sendPresenceUpdate('composing', m.chat);
+  client.sendPresenceUpdate('recording', m.chat);
     }
     
 
