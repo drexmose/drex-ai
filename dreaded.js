f6c647c4830ec1f8658d50c8f01a0ffb2024bbb4
drexmose <155102908+drexmose@users.noreply.github.com>
2023-12-30 11:42:13 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -48,7 +48,7 @@ const Heroku = require("heroku-client");
 const gptdm = process.env.GPT_INBOX || 'TRUE';
     const cmd = body.startsWith(prefix);
 //const autobio = process.env.AUTOBIO || 'TRUE';
-const botname = process.env.BOTNAME || 'CROWN AI';
+const botname = process.env.BOTNAME || 'DREX AI';
 const antibot = process.env.ANTIBOT || 'FALSE';
   
     const command = body.replace(prefix, "").trim().split(/ +/).shift().toLowerCase();
