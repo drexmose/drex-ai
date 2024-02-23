70347c24c79d9d3b713795dc903fba1d5863ff58
drexmose <155102908+drexmose@users.noreply.github.com>
2024-02-23 16:34:38 +0300

    𝐃𝐑𝐄𝐗 𝐁𝐎𝐓

--- dreaded.js
+++ dreaded.js
@@ -654,6 +654,71 @@ break;
  }
  break; 
 
+ 'playa': {
+ if (!text) {
+ reply('𝐏𝐫𝐨𝐯𝐢𝐝𝐞 𝐚 𝐬𝐞𝐚𝐫𝐜𝐡 𝐭𝐞𝐫𝐦!\n𝐄.𝐠: 𝙿𝚕𝚊𝚢 𝚂𝚑𝚞𝚜𝚑𝚊 𝙽𝚢𝚊𝚟𝚞 𝚋𝚢 𝙲𝚑𝚛𝚒𝚜𝚝𝚒𝚗𝚊 𝚂𝚞𝚜𝚑𝚘')
+ return;
+ }
+ try {
+ const {
+ videos
+ } = await yts(text);
+ if (!videos || videos.length <= 0) {
+ reply(`No Matching videos found for : *${args[0]}*!!`)
+ return;
+ }
+ let urlYt = videos[0].url
+ let infoYt = await ytdl.getInfo(urlYt);
+ //30 MIN
+ if (infoYt.videoDetails.lengthSeconds >= 1800) {
+ reply(`Too big!\I'm Unable to download big files. 🤥`);
+ return;
+ }
+ const getRandonm = (ext) => {
+ return `${Math.floor(Math.random() * 10000)}${ext}`;
+ };
+ let titleYt = infoYt.videoDetails.title;
+ let randomName = getRandonm(".mp3");
+ const stream = ytdl(urlYt, {
+ filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
+ })
+ .pipe(fs.createWriteStream(`./${randomName}`));
+ console.log("Audio downloading ->", urlYt);
+ // reply("Downloading.. This may take up to 5 min!");
+ await new Promise((resolve, reject) => {
+ stream.on("error", reject);
+ stream.on("finish", resolve);
+ });
+ 
+ let stats = fs.statSync(`./${randomName}`);
+ let fileSizeInBytes = stats.size;
+ // Convert the file size to megabytes (optional)
+ let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+ console.log("Audio downloaded ! \n Size: " + fileSizeInMegabytes);
+ if (fileSizeInMegabytes <= 40
+{
+         await client.sendmessage(
+from, {
+audio:
+fs.readFileSync(`./${randomName}`),
+          mimetype: "audio/mpeg",
+          ptt: false
+       }, {
+          quoted: fcontact 
+         } 
+       );
+} else {
+reply(`File size too big to send as audio.`);
+}
+ fs.unlinkSync(`./${randomName}`);
+ } catch (e) {
+ reply(e.toString())
+ }
+ }
+break;
+
+		      
+
 	      case 'hd': case 'hdr': case 'remini': {
 			if (!quoted) return reply(`Where is the picture?`)
 			if (!/image/.test(mime)) return reply(`Send/Reply Photos With Captions ${prefix + command}`)
