d1b60466376da504d58d22da1736b8d6304f729d
drexmose <155102908+drexmose@users.noreply.github.com>
2024-02-24 10:59:01 +0300

    𝐃𝐑𝐄𝐗 𝐁𝐎𝐓

--- dreaded.js
+++ dreaded.js
@@ -92,7 +92,6 @@ const menu = process.env.MENU_TYPE || 'VIDEO';
     const thum = fs.readFileSync ('./𝐃𝐑𝐄𝐗_𝐀𝐈.jpg');	  
     const Owner = DevDreaded.map((v) => v.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(m.sender);
     const viewOnceMessage = args.join(" ");
-    const groupAdmins = m.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
     const fcontact = { key: {fromMe: false,participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { 'contactMessage': { 'displayName': `𝐃𝐑𝐄𝐗 𝐁𝐎𝐓`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;DrexBot,;;;\nFN:${pushname},\nitem1.TEL;waid=${sender.split('@')[0]}:${sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': { url: 'https://telegra.ph/file/00c44dbca471eff754425.png' }}}}
        // Group
     const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch((e) => {}) : "";
@@ -604,10 +603,10 @@ break;
         let teks = `*「 Tag Admins 」*
 
 *Message : ${text}*\n\n`
-        for (let mem of groupAdmins) {
+        for (let mem of getGroupAdmins) {
           teks += `🍁 @${mem.split('@')[0]}\n`
         }
-        client.sendMessage(m.chat, { text: teks, mentions: groupAdmins }, { quoted: fcontact })
+        client.sendMessage(m.chat, { text: teks, mentions: getGroupAdmins }, { quoted: fcontact })
       }
         break;
 
